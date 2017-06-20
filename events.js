a953a12a50adca2c00b5b5b17ae26fba3d08141b
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-20 08:15:38 -0500

    working on mouseon/hover event

--- events.js
+++ events.js
@@ -15,26 +15,26 @@ articleEl[0].addEventListener("click", handleSectionClick);
 
 function sayYourName(name){
 	console.log("My name is"+ name );
-}
+};
 
 let piggie = document.getElementById("guinea-pig");
 
 document.getElementById("add-color").addEventListener("click", function(){
 	piggie.classList.toggle("blue");
 	console.log(piggie, piggie.classList);
-}());
+});
 
 document.getElementById("make-large").addEventListener("click", function(){
 	piggie.classList.toggle("bold");
 	console.log(piggie, piggie.classList);
-}());
+});
 
 document.getElementById("add-border").addEventListener("click", function(){
 	piggie.classList.toggle("border");
 	console.log(piggie, piggie.classList);
-}());
+});
 
 document.getElementById("add-rounding").addEventListener("click", function(){
-	piggie.classList.toggle("rounded");s
+	piggie.classList.toggle("rounded");
 	console.log(piggie, piggie.classList);
-}());
+});
